62eb411d940a125b2f1ac2fd56a9419454c78508
Lina C <linaclark96@gmail.com>
2018-01-21 09:33:41 -0500

    map update

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Header from "./components/Header";
 import Map from "./components/Map";
 import Location from "./components/Location";
-import MapGL from 'react-map-gl';
 import './App.css';
 
 class App extends Component {
@@ -11,16 +10,7 @@ class App extends Component {
       <div className="App">
       <Header />
       <Location />
-      <MapGL
-      width={700}
-      height={450}
-      latitude={37.768}
-      longitude={-122.331}
-      zoom={9.017}
-      mapStyle="mapbox://styles/mapbox/dark-v9" 
-      mapboxApiAccessToken={Your_Mapbox_Token}
-     />
-      
+      <Map />
       </div>
     );
   }
