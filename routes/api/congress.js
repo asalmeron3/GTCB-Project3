0a0236a57dfe9849e2e58ca463b809726e3728f2
YU JUNG JUNG <i.respectyou.alot@gmail.com>
2018-01-25 15:32:50 -0500

    route update

--- routes/api/congress.js
+++ routes/api/congress.js
@@ -1,3 +1,5 @@
+
+
 $.ajax({
          url: "https://api.propublica.org/congress/v1/members/senate/RI/current.json",
          type: "GET",
@@ -6,3 +8,29 @@ $.ajax({
        }).done(function(data){
        console.log(data)
        });
+
+
+<Request
+  instance={axios.create({})} /* custom instance of axios - optional */
+  method="get" /* get, delete, head, post, put and patch - required */
+  url="https://api.propublica.org/congress/v1/members/senate/RI/current.json" /*  url endpoint to be requested - required */
+  data={} /* post data - optional */
+  params={} /* queryString data - optional */
+  config={} /* axios config - optional */
+  debounce={200} /* minimum time between requests events - optional */
+  debounceImmediate={true} /* make the request on the beginning or trailing end of debounce - optional */
+  isReady={true} /* can make the axios request - optional */
+  onSuccess={(response)=>{}} /* called on success of axios request - optional */
+  onLoading={()=>{}} /* called on start of axios request - optional */
+  onError=(error)=>{} /* called on error of axios request - optional */
+/>
+
+//axios
+axios({
+  method:'get',
+  url:'https://newsapi.org/v2/top-headlines?q=donaltrump&country=us&apiKey=15b928baec3648afbf5fe290acd1df58',
+  responseType:'stream'
+}).then(function(response) {
+  
+  
+});
