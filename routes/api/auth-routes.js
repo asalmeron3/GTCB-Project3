bf01d035fea1c6cf3f9329bdaf285323deb4cb1f
Lina C <linaclark96@gmail.com>
2018-02-10 17:50:42 -0500

    rebuilding

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -94,9 +94,21 @@ router.post('/pic', function(req, res){
   });
 });
 
-router.get('/location', function(req, res) {
-  User.findOne({ "location":req.body.location })
-  .populate('bills');
+app.get("/location", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  User
+    .findOne({username: globalUser})
+    // ..and populate all of the notes associated with it
+    .populate("bills")
+    .then(function(userInfo) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(userInfo);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
+
 
 module.exports = router;
