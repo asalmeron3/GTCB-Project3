0efbc8c652a47db4ace1de09323898ddf8cb077e
Lina C <linaclark96@gmail.com>
2018-02-10 13:32:44 -0500

    rebuilding

--- controllers/billsController.js
+++ controllers/billsController.js
@@ -18,7 +18,7 @@ module.exports = {
   create: function(req, res) {
     db.Bill
     console.log(req.body)
-    console.log(req)  
+    //console.log(req)  
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
