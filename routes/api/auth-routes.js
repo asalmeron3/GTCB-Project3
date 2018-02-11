854f9f10de16036c79c7c4cd736d1adf0a9c2349
Lina C <linaclark96@gmail.com>
2018-02-11 17:36:57 -0500

    rebuilding

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -85,7 +85,7 @@ router.post('/location', function(req, res) {
 });
 
 router.post('/pic', function(req, res){
-  User.findOneAndUpdate({ username: globalUser }, {$set: {"picURL": req.body.picURL}}, function(err, doc){
+  User.findOneAndUpdate({ username: globalUser }, {$set: {"UserPic": req.body.picURL}}, function(err, doc){
     if(err){
       return res.json(err);
     } else{
@@ -94,21 +94,14 @@ router.post('/pic', function(req, res){
   });
 });
 
-router.get("/location", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  User
-    .findOne({username: globalUser})
-    // ..and populate all of the notes associated with it
-    .populate("bills")
-    .then(function(userInfo) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(userInfo);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+router.get('/location', function(req, res) {
+  User.findOne({ "location":req.body.location }, function(err, doc){
+    if(err){
+      return res.json(err);
+    } else{
+     res.json(doc);
+    }
+  });
 });
-
 
 module.exports = router;
