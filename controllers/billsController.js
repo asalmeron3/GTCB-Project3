2ba8a9fb45398d191604f21d088b7c33d5989b17
Arturo Salmeron <asalmeron3@gatech.edu>
2018-02-15 14:15:42 -0500

    Hail Mary on these Bills

--- controllers/billsController.js
+++ controllers/billsController.js
@@ -3,7 +3,7 @@ const db = require("../models");
 // Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
-    db.Users.bills
+    db.Bill
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
